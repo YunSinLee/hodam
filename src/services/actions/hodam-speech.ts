be48dcc76634d781940dd724828a4cf04ef80e11
LeeYunSin <dldbstls7777@naver.com>
2024-03-18 22:40:29 +0900

    chore: hodamSpeech(tts) 적용

---
+++ src/services/actions/hodam-speech.ts
@@ -0,0 +1,43 @@
+import Hodam from "@/app/hodam/page";
+
+export function useHodamSpeech() {
+  const HodamSpeech = new SpeechSynthesisUtterance();
+
+  changeHodamSpeechSetting({
+    lang: "ko-KR",
+    volume: 1,
+    rate: 0.6,
+    pitch: 0.75,
+  });
+
+  async function changeHodamSpeechSetting({
+    lang,
+    volume,
+    rate,
+    pitch,
+  }: {
+    lang?: string;
+    volume?: number;
+    rate?: number;
+    pitch?: number;
+  }) {
+    if (lang) HodamSpeech.lang = lang;
+    if (volume) HodamSpeech.volume = volume;
+    if (rate) HodamSpeech.rate = rate;
+    if (pitch) HodamSpeech.pitch = pitch;
+  }
+
+  function setText(text: string) {
+    HodamSpeech.text = text;
+  }
+
+  function speak() {
+    window.speechSynthesis.speak(HodamSpeech);
+  }
+
+  function stop() {
+    window.speechSynthesis.cancel();
+  }
+
+  return { HodamSpeech, changeHodamSpeechSetting, setText, speak, stop };
+}
