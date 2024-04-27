58f596348386a927bb1a881b52a1b29f97d15243
LeeYunSin <dldbstls7777@naver.com>
2024-04-27 15:25:54 +0900

    chore: 내 동화 테이블 개선

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 /**
  * 주어진 값이 논리적인(JS 문법이 아닌) Empty 상태인지 반환합니다.
  * @param {unknown} value - 검사값
@@ -10,3 +12,7 @@ export const isEmpty = (value: unknown) => {
   if (typeof value === "object") return Object.keys(value).length === 0;
   return false;
 };
+
+export function formatTime(time: string, format: string = "YYYY-MM-DD HH:mm") {
+  return dayjs(time).format(format);
+}
