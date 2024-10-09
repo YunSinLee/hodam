083af9068c5ee664371cd724f0daa1d20e034ace
LeeYunSin <dldbstls7777@naver.com>
2024-10-09 19:11:04 +0900

    chore: primereact unstyled:false 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import { PrimeReactProvider } from "primereact/api";
-import "../styles/globals.css";
 import "primereact/resources/themes/lara-light-amber/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import NavBar from "./components/NavBar";
 import { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
+
+import "../styles/globals.css";
+
 export const metadata: Metadata = {
   title: {
     template: "%s | 내가 만드는 동화, 호담",
@@ -23,7 +25,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <PrimeReactProvider value={{ ripple: true, unstyled: true }}>
+      <PrimeReactProvider value={{ ripple: true }}>
         <body>
           <div className="h-navbar fixed top-0 z-10 w-full  border-b-2 border-gray-300 bg-white">
             <NavBar />
